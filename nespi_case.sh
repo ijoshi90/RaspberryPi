917237c18224878639794036248ccfdc687e2f30
Akshay Joshi <akshay.joshi.m@gmail.com>
2019-09-11 20:17:51 +0530

    Update nespi_case.sh

--- nespi_case.sh
+++ nespi_case.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+# NESPi Case - Shutdown & Reset Button Scripts
+echo "Getting the latest safe shutdown script, this will reboot the system after install..."
+sleep 3
+wget -O - "https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master/install.sh" | sudo bash
